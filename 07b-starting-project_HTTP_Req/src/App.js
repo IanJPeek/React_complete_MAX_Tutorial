f5ece890a847b08ddbb34069b110c109ea174ef8
IanJPeek <ianp@apadmi.com>
2021-05-13 17:24:45 +0100

    Added error handling

--- 07b-starting-project_HTTP_Req/src/App.js
+++ 07b-starting-project_HTTP_Req/src/App.js
@@ -7,21 +7,35 @@ function App() {
 
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   async function fetchMoviesHandler() {
     setIsLoading(true)
-    const response = await fetch("https://swapi.dev/api/films/")
-    const data = await response.json()
+    setError(null)
 
-    const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
+    try {
+      const response = await fetch("https://swapi.dev/api/films/")
+
+      // Fetch API doesn't throw errors for status codes (unlike eg Axios), so manually throwing error here 
+      // - could also check eg response.status for actual error code
+      if (!response.ok) {
+        throw new Error(`Something went wrong! ${response.status} error`)
       }
-    })
-    setMovies(transformedMovies)
+
+      const data = await response.json()
+
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformedMovies)
+    } catch (error) {
+      setError(error.message)
+    }
     setIsLoading(false)
   }
 
@@ -32,8 +46,9 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
         {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p> }
       </section>
     </React.Fragment>
   );
